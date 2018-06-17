32b7943e71c372f17c1e1e8de3abdb8d9c0b79fc
Domenico Giambra <zorinik@gmail.com>
2018-06-17 20:45:50 +0200

    Now uses cryptographically secure function "random_bytes"

--- RandToken.php
+++ RandToken.php
@@ -5,22 +5,13 @@ use Model\Core\Module;
 class RandToken extends Module
 {
 	/**
-	 * @param string $seed
+	 * @param string $idx
 	 * @return string
 	 */
-	public function getToken(string $seed): string
+	public function getToken(string $idx): string
 	{
-		if (!isset($_SESSION[SESSION_ID]['randToken-' . $seed]) or !is_string($_SESSION[SESSION_ID]['randToken-' . $seed]) or strlen($_SESSION[SESSION_ID]['randToken-' . $seed]) != 20) {
-			mt_srand(array_sum(array_map(function ($l) {
-				return ord($l);
-			}, str_split($seed))));
-
-			$_SESSION[SESSION_ID]['randToken-' . $seed] = '';
-			for ($c = 1; $c <= 20; $c++) {
-				$array_for_randomness = [mt_rand(48, 57), mt_rand(65, 90), mt_rand(97, 122)];
-				$_SESSION[SESSION_ID]['randToken-' . $seed] .= chr($array_for_randomness[array_rand($array_for_randomness)]);
-			}
-		}
-		return $_SESSION[SESSION_ID]['randToken-' . $seed];
+		if (!isset($_SESSION[SESSION_ID]['randToken-' . $idx]) or !is_string($_SESSION[SESSION_ID]['randToken-' . $idx]) or strlen($_SESSION[SESSION_ID]['randToken-' . $idx]) != 20)
+			$_SESSION[SESSION_ID]['randToken-' . $idx] = random_bytes(20);
+		return $_SESSION[SESSION_ID]['randToken-' . $idx];
 	}
 }
--- tests/RandTokenTest.php
+++ tests/RandTokenTest.php
@@ -24,17 +24,17 @@ class RandTokenTest extends TestCase
 	{
 		$randToken = new Model\RandToken\RandToken($this->getModelCore());
 
-		// Retrieves a token with a seed
+		// Retrieves a token with an index
 		$token = $randToken->getToken('Foo');
 		$this->assertInternalType('string', $token);
 		$this->assertEquals(strlen($token), 20);
 
-		// Retrieves another token with the same seed, should be equal to the previous one
+		// Retrieves another token with the same index, should be equal to the previous one
 		$token2 = $randToken->getToken('Foo');
 		$this->assertInternalType('string', $token2);
 		$this->assertEquals($token, $token2);
 
-		// Retrieves another token with a new seed, should be different than the previous ones
+		// Retrieves another token with a new index, should be different than the previous ones
 		$token3 = $randToken->getToken('Bar');
 		$this->assertInternalType('string', $token3);
 		$this->assertEquals(strlen($token3), 20);
