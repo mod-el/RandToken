182f9b6d5cc276d0373b28d3ea21c911d2191403
Domenico Giambra <zorinik@gmail.com>
2018-06-18 00:23:05 +0200

    Fixed token length

--- RandToken.php
+++ RandToken.php
@@ -11,7 +11,7 @@ class RandToken extends Module
 	public function getToken(string $idx): string
 	{
 		if (!isset($_SESSION[SESSION_ID]['randToken-' . $idx]) or !is_string($_SESSION[SESSION_ID]['randToken-' . $idx]) or strlen($_SESSION[SESSION_ID]['randToken-' . $idx]) != 20)
-			$_SESSION[SESSION_ID]['randToken-' . $idx] = bin2hex(random_bytes(20));
+			$_SESSION[SESSION_ID]['randToken-' . $idx] = bin2hex(random_bytes(10));
 		return $_SESSION[SESSION_ID]['randToken-' . $idx];
 	}
 }
